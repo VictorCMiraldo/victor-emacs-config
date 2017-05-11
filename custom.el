170ed3421f028ab81ab2effd25c68f9866d08edf
Victor <v.cacciarimiraldo@gmail.com>
2017-05-11 15:36:12 +0200

    evil-shift-width = 2

--- custom.el
+++ custom.el
@@ -13,6 +13,7 @@
     ("7a4efa993973000e5872099a3c24c310b8bb2568b70f3b9d53675e6edf1f3ce4" default)))
  '(custom-theme-directory "~/.emacs.d/themes/")
  '(evil-auto-indent nil)
+ '(evil-shift-width 2)
  '(evil-snipe-scope (quote visible))
  '(global-linum-mode t)
  '(haskell-interactive-popup-errors nil)
