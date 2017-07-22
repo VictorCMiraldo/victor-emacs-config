16c29d95bc1f53bcfdfee0b990b5cdbcd4db2eea
victor <v.cacciarimiraldo@gmail.com>
2017-07-22 14:10:38 -0400

    installed time machine

---
+++ elpa/git-timemachine-3.0/git-timemachine-pkg.el
@@ -0,0 +1,1 @@
+(define-package "git-timemachine" "3.0" "Walk through git revisions of a file" 'nil :commit "7c66a878ee89861dcd59b5dfc598520daa156052" :url "https://github.com/pidu/git-timemachine" :keywords '("git"))
