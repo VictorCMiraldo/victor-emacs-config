ae0ecc7193512a2de165c2a8a12128762e1a0374
victor <v.cacciarimiraldo@gmail.com>
2017-10-07 13:22:27 +0200

    Added proof general to the mix

--- local/fstar.el
+++ local/fstar.el
@@ -1,4 +1,7 @@
 ;; Basic f* configuration
 
+;; Enters f-star mode
 (add-to-list 'auto-mode-alist '("\\.fsi\\'" . fstar-mode))
 
+;; loads proof general
+(load "/home/victor/.opam/system/share/proofgeneral/generic/proof-site.el")
