afe95951887eaae732f34836ca4cddbba10319d9
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2019-10-03 09:43:11 +0200

    We also gotta let AUCTex create the 'auto' directory

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-auto-save t)
  '(TeX-one-master "\\.\\(texi?\\|dtx\\|lhs\\)$")
  '(ac-auto-start nil)
  '(ac-delay 0.4)
